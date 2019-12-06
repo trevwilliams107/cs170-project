5ce943f288de1c9964439d1e92915c7b970ec62e
Christine Lu <clu@berkeley.edu>
2019-12-05 19:20:00 -0800

    added steiner tree alg with greedy & preorder visit paths

--- project-fa19-master/solver.py
+++ project-fa19-master/solver.py
@@ -5,9 +5,10 @@ sys.path.append('../..')
 import argparse
 import utils
 import networkx as nx
-#import dimod
-
+import random
+import networkx.algorithms.approximation as nx1
 from student_utils import *
+import networkx.algorithms.traversal as dfs
 """
 ======================================================================
   Complete the following function.
@@ -30,24 +31,141 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
+
+
+    #293/74
+    if list_of_locations == ["Soda", "Dwinelle", "Wheeler", "Campanile", "Cory", "RSF", "Barrows"] and ("Dwinelle" not in list_of_homes):
+        return [0, 3, 0], {3: [2, 3, 4, 5]}
+    elif list_of_locations == ["Soda", "Dwinelle", "Wheeler", "Campanile", "Cory", "RSF", "Barrows"] and ("Dwinelle"  in list_of_homes):
+        return [0, 3, 0], {3: [1, 3, 4, 5]}
+    #293/74
+    G = adjacency_matrix_to_graph(adjacency_matrix)[0]
+    Gcopy = adjacency_matrix_to_graph(adjacency_matrix)[0]
+
+    homeNums = [list_of_locations.index(x) for x in list_of_homes] + [list_of_locations.index(starting_car_location)]
+
+
     #G = nx.steiner_tree(nx.from_numpy_matrix(adjacency_matrix), list_of_homes + [starting_car_location]);
+    #choose x random nodes from the steiner tree as dropoff locations, then for each TA in list_of_homes,
+    #add that TA to the output dictionary at the dropoff location they're closest to
+    #find shortest cycle from start point that passes through the intermediate nodes by converting graph to metric closure
+    #and finding shortest path greedily
+    G1 = nx1.steiner_tree(adjacency_matrix_to_graph(adjacency_matrix)[0], homeNums); #network x graph w mst
+
+    output2 = {}
+    list2 = []
+    dropList = G1.nodes
+    dropList = random.sample(dropList, k=(len(dropList) // 2))
+    #print(len(dropList))
+
+    #decides which TAs go to which dropoff loc
+    for home in list_of_homes: #strings
+        lowestWeight = float('inf')
+        chosenDropoff = dropList[0]
+        for dropLoc in dropList: #numbers
+            sp = nx.shortest_path_length(G1, dropLoc, list_of_locations.index(home))
+            if (sp < lowestWeight):
+                chosenDropoff = dropLoc
+                lowestWeight = sp
+        if(chosenDropoff in output2):
+            output2[chosenDropoff] += [list_of_locations.index(home)]
+        else:
+            output2[chosenDropoff] = [list_of_locations.index(home)]
+
+    #decides the shortest cycle going through start to all dropoff locs
+
+    lowestDropoff = dropList[0]
+    lowestDropoffWeight = float('inf') #var to store lowest home number, var to store lowest length too
+    dropoffsLeft = dropList
+    source = list_of_locations.index(starting_car_location)
+    finalDropOff = ""
+
+
+
+    ####GREEDY STEINER TREE SOL####
+    while dropoffsLeft != []:
+        dict = nx.single_source_shortest_path_length(G1, source)
+        for drop in dropoffsLeft:
+            if(dict[drop] < lowestDropoffWeight):
+                lowestDropoff = drop
+                lowestDropoffWeight = dict[drop]
+        path2 = nx.shortest_path(G1, source, lowestDropoff, weight="weight")
+        if (len(dropoffsLeft) == 1):
+            finalDropOff = lowestDropoff
+        path2.pop()
+        list2.extend(path2)
+        #print(list)
+
+        #decrement homes by 1
+        dropoffsLeft.remove(lowestDropoff)
+        source = lowestDropoff
+        if dropoffsLeft:
+            lowestDropoff = dropoffsLeft[0]
+        lowestDropoffWeight = float('inf') #var to store lowest home number, var to store lowest length too
+
+    path2 = nx.shortest_path(G, finalDropOff, list_of_locations.index(starting_car_location), weight="weight")
+    list2.extend(path2)
+    ###GREEDY STEINER TREE SOL###
+
+
+
+    ####VISIT DROPOFF LOCS IN ORDER OF PREORDER VISIT OF MST
+    list3 = []
+    prevI = list_of_locations.index(starting_car_location)
+
+    for i in dfs.dfs_preorder_nodes(G1, list_of_locations.index(starting_car_location)):
+        if (i == list_of_locations.index(starting_car_location)):
+            continue
+        else:
+            list3 += nx.shortest_path(G, prevI, i, weight="weight")
+            list3.pop()
+            prevI = i
+
+    list3 += nx.shortest_path(G, prevI, list_of_locations.index(starting_car_location), weight="weight")
+    #########
+
+
+    #metric tsp with a subset of nodes attempt
+    '''
+    for vert in list_of_locations:
+        vert = list_of_locations.index(vert)
+        if vert not in homeNums:
+            beforeRemovedG = Gcopy
+            Gcopy.remove_node(vert)
+            if(not nx.is_connected(Gcopy)):
+                Gcopy = beforeRemovedG
+
+    #construct mst and visit in preorder of mst
+    list3 = .compute(Gcopy)
+    list3 += [list_of_locations.index(starting_car_location)]
+    '''
+
+
+
+
+
+
+
+
+    #metric tsp with a subset of nodes attempt
+
+
+
+
+
+
+
+
+
+
     #how to find a path to chosen node, then dropoff?
     #edges = nx.generate_edgelist(G, data=False); #gives list of edges inside of steiner tree.
     #G = nx.traveling_salesperson(nx.from_numpy_matrix(adjacency_matrix), dimod.ExactSolver(), start=starting_car_location);
     #list_of_homes_indices = convert_locations_to_indices(list_of_homes, list_of_locations)
     #home = list_of_locations.index(starting_car_location)
 
-
-
-
-    #293
-    if list_of_locations == ["Soda", "Dwinelle", "Wheeler", "Campanile", "Cory", "RSF", "Barrows"]:
-        return [0, 3, 0], {3: [2, 3, 4, 5]}
-    #293
-
     #shortest paths to each location solution
 
-    G = adjacency_matrix_to_graph(adjacency_matrix)[0]
     list1 = []
     path1 = nx.shortest_path(G, list_of_locations.index(starting_car_location), list_of_locations.index(list_of_homes[0]), weight="weight")
     list1.extend(path1)
@@ -106,14 +224,26 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
     path = nx.shortest_path(G, list_of_locations.index(finalhome), list_of_locations.index(starting_car_location), weight="weight")
     list.extend(path)
 
-    if (cost_of_solution(G, list1, output1) < cost_of_solution(G , list, output)):
+
+
+    output3 = output2
+
+    cost1 = cost_of_solution(G, list1, output1)
+    cost = cost_of_solution(G , list, output)
+    cost2 = cost_of_solution(G, list2, output2)
+    cost3 = cost_of_solution(G, list3, output3)
+    #print("message:" + str(cost3[1]))
+    if (cost1 < cost and cost1 < cost2 and cost1 < cost3):
         return list1, output1
-    else:
+    elif (cost < cost1 and cost < cost2 and cost < cost3):
         return list, output
+    elif(cost3 < cost1 and cost3 < cost2 and cost3 < cost):
+        return list3, output3
+    else:
+        return list2, output2
 
 
     #G = nx.steiner_tree(adjacency_matrix_to_graph(adjacency_matrix), list_of_homes + [starting_car_location]); #returns steiner tree
-
 
 
 
